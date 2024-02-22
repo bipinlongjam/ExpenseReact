4f6d1a60f19cddc3410c402bb446bf79187a9f3d
bipinlongjam <bipinlongjam11@gmail.com>
2024-02-23 00:03:29 +0530

    add changeAmount button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,19 +4,24 @@ import Card from '../UI/Card';
 import { useState } from 'react';
 
 const ExpenseItem = (props)=>{
+    const [expenseAmount, setExpenseAmount] = useState(props.amount);
     const [isVisible, setIsVisible] = useState(true)
     const clickHandler =() =>{
         setIsVisible(false)
     }
+    const handleAmountChange = () => {
+        setExpenseAmount(100); // Change the expense amount to $100
+      };
     return isVisible ?(
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                <h2>{props.title}</h2> 
-               <div className='expense-item__price'>Rs: {props.amount}</div>
+               <div className='expense-item__price'>Rs:{expenseAmount}</div>
                <div className='expense-item__location'>Loaction: {props.location}</div>
             </div>
             <button onClick={clickHandler}>Delete</button>
+            <button onClick={handleAmountChange}>Change Amount</button>
         </Card>
     ): null
 }
