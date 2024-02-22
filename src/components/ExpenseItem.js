b7f7ac55e95a7057dea0617afb89ee296a013dc6
bipinlongjam <bipinlongjam11@gmail.com>
2024-02-22 14:06:09 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return(
+        <div>
+        <h1>Expense Items</h1>
+        <div>
+        <h3>Date : 22 feb 2024</h3>
+        <ul>
+        <li> Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+       </ul>
+        </div>
+       
+        </div>
+    )
+}
+export default ExpenseItem;
