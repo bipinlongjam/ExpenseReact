86c6dc44ceccca3485c94c50b2f0f3708e988a92
bipinlongjam <bipinlongjam11@gmail.com>
2024-02-22 22:48:51 +0530

    add delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,23 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 const ExpenseItem = (props)=>{
-   
-    return(
-            <Card className="expense-item">
+    const [isVisible, setIsVisible] = useState(true)
+    const clickHandler =() =>{
+        setIsVisible(false)
+    }
+    return isVisible ?(
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                <h2>{props.title}</h2> 
                <div className='expense-item__price'>Rs: {props.amount}</div>
                <div className='expense-item__location'>Loaction: {props.location}</div>
             </div>
-            </Card>
-        
-    
-    )
+            <button onClick={clickHandler}>Delete</button>
+        </Card>
+    ): null
 }
 export default ExpenseItem;
